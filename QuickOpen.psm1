f2555884ae289a4faace4ecbdbb0ad0d79ec0107
ztytotoro <ztytotoro@outlook.com>
2017-09-14 16:07:38 +0800

    init

---
+++ QuickOpen.psm1
@@ -0,0 +1,60 @@
+function open {
+    $t = ("*" + ($args -join "*") + "*")
+    $s = Get-Item (Get-Content $HOME/psconfig/quickopen.txt)
+    foreach ($item in $s) {
+        if ($item.Name -like $t){
+            if($item.PsIsContainer){
+                explorer.exe $item.FullName;
+            }else{
+                explorer.exe $item.Directory.Parent.FullName;
+            }
+        }
+    }
+}
+
+function pin {
+    $s = (Get-Content $HOME/psconfig/quickopen.txt)
+    $t = New-Object System.Collections.ArrayList
+    
+    $Add = ($args -join " ")
+
+    foreach ($item in $s) {
+        $t.Add($item) | Out-Null
+    }
+
+    if($Add){
+        $t.Add($Add) | Out-Null
+    }
+    Set-Content -Path $HOME/psconfig/quickopen.txt -Value $t;
+}
+
+function unpin {
+    $s = (Get-Content $HOME/psconfig/quickopen.txt)
+    $t = New-Object System.Collections.ArrayList
+    
+    $Remove = ($args -join " ")
+
+    foreach ($item in $s) {
+        $t.Add($item) | Out-Null
+    }
+
+    if($Remove){
+        $t.Remove($Remove) | Out-Null
+    }
+    Set-Content -Path $HOME/psconfig/quickopen.txt -Value $t;
+}
+
+function pined {
+    $t = ("*" + ($args -join "*") + "*")
+    if($t){ 
+        $s = Get-Item (Get-Content $HOME/psconfig/quickopen.txt)
+        foreach ($item in $s) {
+            if ($item.Name -like $t){
+                $item.FullName;
+            }
+        }
+    }
+    else{
+        Write-Host -ForegroundColor 2 (Get-Content $HOME/psconfig/quickopen.txt)
+    }
+}
